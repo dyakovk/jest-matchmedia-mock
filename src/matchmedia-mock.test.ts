18f6ca07bf9d6cb6b74314d8b8211d554e86ebb8
Kirill Dyakov <dyak.kir@gmail.com>
2020-02-04 16:17:16 +0300

    feat: create mock implementation of the matchMedia method

---
+++ src/matchmedia-mock.test.ts
@@ -0,0 +1,166 @@
+import MatchMediaMock from './matchmedia-mock';
+
+let matchMedia: MatchMediaMock;
+
+const appearanceMq = {
+  light: '(prefers-color-scheme: light)',
+  dark: '(prefers-color-scheme: dark)',
+};
+
+describe('MatchMedia Mock', () => {
+  beforeAll(() => {
+    matchMedia = new MatchMediaMock();
+  });
+
+  afterEach(() => {
+    matchMedia.clear();
+  });
+
+  describe('Adding Listeners', () => {
+    test('adds a new listener function for media query', () => {
+      const firstListener = jest.fn();
+      const secondListener = jest.fn();
+      const mql = window.matchMedia(appearanceMq.light);
+
+      mql.addListener(firstListener);
+      mql.addEventListener<'change'>('change', secondListener);
+
+      expect(matchMedia.getListeners(appearanceMq.light)).toHaveLength(2);
+    });
+
+    test("does not add a listener function for a media query if the event name is not 'change'", () => {
+      const listener = jest.fn();
+      const mql = window.matchMedia(appearanceMq.light);
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      mql.addEventListener<'click'>('click', listener);
+
+      expect(matchMedia.getListeners(appearanceMq.light)).toHaveLength(0);
+    });
+
+    test('does not add the same listener function twice for media query', () => {
+      const listener = jest.fn();
+      const mql = window.matchMedia(appearanceMq.light);
+
+      mql.addListener(listener);
+      mql.addEventListener<'change'>('change', listener);
+
+      expect(matchMedia.getListeners(appearanceMq.light)).toHaveLength(1);
+    });
+  });
+
+  describe('Removing Listeners', () => {
+    test('removes a listener function previously added for media query', () => {
+      const firstListener = jest.fn();
+      const secondListener = jest.fn();
+      const mql = window.matchMedia(appearanceMq.light);
+
+      mql.addListener(firstListener);
+      mql.addEventListener('change', secondListener);
+
+      expect(matchMedia.getListeners(appearanceMq.light)).toHaveLength(2);
+
+      mql.removeListener(firstListener);
+      mql.removeEventListener('change', secondListener);
+
+      expect(matchMedia.getListeners(appearanceMq.light)).toHaveLength(0);
+    });
+
+    test("does not remove a listener function for a media query if the event name is not 'change'", () => {
+      const listener = jest.fn();
+      const mql = window.matchMedia(appearanceMq.light);
+
+      mql.addEventListener<'change'>('change', listener);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      mql.removeEventListener<'click'>('click', listener);
+
+      expect(matchMedia.getListeners(appearanceMq.light)).toHaveLength(1);
+    });
+
+    test('does not remove a listener function for unkwown media query', () => {
+      const listener = jest.fn();
+
+      expect(matchMedia.getMediaQueries()).not.toContain(listener);
+      window.matchMedia(appearanceMq.dark).removeListener(listener);
+      expect(matchMedia.getMediaQueries()).not.toContain(listener);
+    });
+
+    test('does not remove the same listener function twice for media query', () => {
+      const firstListener = jest.fn();
+      const secondListener = jest.fn();
+      const mql = window.matchMedia(appearanceMq.light);
+
+      mql.addListener(firstListener);
+      mql.addEventListener<'change'>('change', secondListener);
+
+      expect(matchMedia.getListeners(appearanceMq.light)).toHaveLength(2);
+
+      mql.removeListener(firstListener);
+      mql.removeEventListener<'change'>('change', firstListener);
+
+      expect(matchMedia.getListeners(appearanceMq.light)).toHaveLength(1);
+      expect(matchMedia.getListeners(appearanceMq.light)).toContain(secondListener);
+    });
+  });
+
+  describe('Calling Listeners', () => {
+    test('throws an error when an applicable media query is not a string', () => {
+      expect(() => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        matchMedia.useMediaQuery(true);
+      }).toThrow();
+    });
+
+    test('performs a one-time instant check when applying a media query, but before adding listener functions', () => {
+      matchMedia.useMediaQuery(appearanceMq.light);
+
+      expect(window.matchMedia(appearanceMq.light).matches).toBeTruthy();
+    });
+
+    test('calls listener functions when applying a media query with previously registered listeners', () => {
+      const firstListener = jest.fn();
+      const secondListener = jest.fn();
+
+      const mql = window.matchMedia(appearanceMq.light);
+
+      mql.addListener(ev => ev.matches && firstListener());
+      mql.addListener(ev => ev.matches && secondListener());
+
+      matchMedia.useMediaQuery(appearanceMq.light);
+
+      expect(firstListener).toBeCalledTimes(1);
+      expect(secondListener).toBeCalledTimes(1);
+    });
+  });
+
+  describe('Clearing and destroying', () => {
+    test('clears all registered media queries and their listeners', () => {
+      const firstListener = jest.fn();
+      const secondListener = jest.fn();
+      const mql = window.matchMedia(appearanceMq.light);
+
+      mql.addListener(firstListener);
+      mql.addEventListener<'change'>('change', secondListener);
+
+      expect(matchMedia.getMediaQueries()).toHaveLength(1);
+      expect(matchMedia.getListeners(appearanceMq.light)).toHaveLength(2);
+
+      matchMedia.clear();
+
+      expect(matchMedia.getMediaQueries()).toHaveLength(0);
+      expect(matchMedia.getListeners(appearanceMq.light)).toHaveLength(0);
+    });
+
+    test('destroys the implementation of window.matchMedia', () => {
+      matchMedia.destroy();
+
+      expect(window.matchMedia).toBeUndefined();
+
+      // Restoring the mock in case of reordering tests
+      matchMedia = new MatchMediaMock();
+    });
+  });
+});
---
+++ src/matchmedia-mock.ts
@@ -0,0 +1,144 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+type MediaQueryListener = ((this: MediaQueryList, ev: MediaQueryListEvent) => any) | null;
+
+export default class MatchMedia {
+  private mediaQueries: {
+    [key: string]: MediaQueryListener[];
+  } = {};
+
+  private mediaQueryList!: MediaQueryList;
+
+  private currentMediaQuery!: string;
+
+  constructor() {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      configurable: true,
+      value: (query: string): MediaQueryList => {
+        const mql: MediaQueryList = {
+          matches: query === this.currentMediaQuery,
+          media: query,
+          onchange: null,
+          /** @deprecated */
+          addListener: listener => {
+            this.addListener(query, listener);
+          },
+          /** @deprecated */
+          removeListener: listener => {
+            this.removeListener(query, listener);
+          },
+          addEventListener: <K extends keyof MediaQueryListEventMap>(
+            type: K,
+            listener: (this: MediaQueryList, ev: MediaQueryListEventMap[K]) => any,
+          ) => {
+            if (type !== 'change') return;
+
+            this.addListener(query, listener);
+          },
+          removeEventListener: <K extends keyof MediaQueryListEventMap>(
+            type: K,
+            listener: (this: MediaQueryList, ev: MediaQueryListEventMap[K]) => any,
+          ) => {
+            if (type !== 'change') return;
+
+            this.removeListener(query, listener);
+          },
+          dispatchEvent: jest.fn(),
+        };
+
+        this.mediaQueryList = mql;
+
+        return mql;
+      },
+    });
+  }
+
+  /**
+   * Adds a new listener function for the specified media query
+   * @private
+   */
+  private addListener(mediaQuery: string, listener: MediaQueryListener): void {
+    if (!this.mediaQueries[mediaQuery]) {
+      this.mediaQueries[mediaQuery] = [];
+    }
+
+    const query = this.mediaQueries[mediaQuery];
+    const listenerIndex = query.indexOf(listener);
+
+    if (listenerIndex !== -1) return;
+    query.push(listener);
+  }
+
+  /**
+   * Removes a previously added listener function for the specified media query
+   * @private
+   */
+  private removeListener(mediaQuery: string, listener: MediaQueryListener): void {
+    if (!this.mediaQueries[mediaQuery]) return;
+
+    const query = this.mediaQueries[mediaQuery];
+    const listenerIndex = query.indexOf(listener);
+
+    if (listenerIndex === -1) return;
+    query.splice(listenerIndex, 1);
+  }
+
+  /**
+   * Updates the currently used media query,
+   * and calls previously added listener functions registered for this media query
+   * @public
+   */
+  public useMediaQuery(mediaQuery: string): never | void {
+    if (typeof mediaQuery !== 'string') throw new Error('Media Query must be a string');
+
+    this.currentMediaQuery = mediaQuery;
+
+    if (!this.mediaQueries[mediaQuery]) return;
+
+    const mqListEvent: Partial<MediaQueryListEvent> = {
+      matches: true,
+      media: mediaQuery,
+    };
+
+    this.mediaQueries[mediaQuery].forEach(listener => {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      listener!.call(this.mediaQueryList, mqListEvent as MediaQueryListEvent);
+    });
+  }
+
+  /**
+   * Returns an array listing the media queries for which the matchMedia has registered listeners
+   * @public
+   */
+  public getMediaQueries(): string[] {
+    return Object.keys(this.mediaQueries);
+  }
+
+  /**
+   * Returns a copy of the array of listeners for the specified media query
+   * @public
+   */
+  public getListeners(mediaQuery: string): MediaQueryListener[] {
+    if (!this.mediaQueries[mediaQuery]) return [];
+    return this.mediaQueries[mediaQuery].slice();
+  }
+
+  /**
+   * Clears all registered media queries and their listeners
+   * @public
+   */
+  public clear(): void {
+    this.mediaQueries = {};
+  }
+
+  /**
+   * Clears all registered media queries and their listeners,
+   * and destroys the implementation of `window.matchMedia`
+   * @public
+   */
+  public destroy(): void {
+    this.clear();
+    delete window.matchMedia;
+  }
+}
