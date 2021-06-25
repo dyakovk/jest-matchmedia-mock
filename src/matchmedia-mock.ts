6177a05bdee709299be7c922f48d618b35785f1c
Sahin Deniz <seahindeniz@gmail.com>
2021-06-26 01:56:55 +0300

    fix(clear): fix mistake

--- src/matchmedia-mock.ts
+++ src/matchmedia-mock.ts
@@ -130,7 +130,7 @@ export default class MatchMedia {
    */
   public clear(): void {
     this.mediaQueries = {};
-    this.currentMediaQuery = 'undefined';
+    this.currentMediaQuery = '';
   }
 
   /**
